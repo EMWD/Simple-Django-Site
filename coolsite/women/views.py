cd452e5a1322cc445f32b63c8c0a47048e16f64b
EMWD <imba2511@yandex.ru>
2021-08-07 15:22:54 +0300

    4

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .forms import *
 from .models import *
