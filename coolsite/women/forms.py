a340c74a7ad009a492eff6a5a4aff5cf550547de
EMWD <imba2511@yandex.ru>
2021-08-08 18:57:24 +0300

    5

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, User
 from django.db.models import fields
 from django.core.exceptions import ValidationError
 from .models import *
@@ -8,8 +9,8 @@ class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['cat'].empty_label = "Категория не выбрана"
-    
-    class Meta:    
+
+    class Meta:
         model = Women
         fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
         widgets = {
@@ -27,30 +28,18 @@ class AddPostForm(forms.ModelForm):
 
 class RemovePostForm(forms.Form):
     title = forms.ModelChoiceField(
-        queryset=Women.objects.all(), 
-        label="Тайтл", 
+        queryset=Women.objects.all(),
+        label="Тайтл",
         empty_label="Ничего не удалять"
     )
 
-# class AddPostForm(forms.ModelForm):
-#     title = forms.CharField(
-#         max_length=255,
-#         label="Заголовок",
-#         widget=forms.TextInput(attrs={'class': 'form-input'})
-#     )
-#     slug = forms.SlugField(max_length=255, label="URL")
-#     content = forms.CharField(
-#         widget=forms.Textarea(
-#         attrs={'cols': 60, 'rows': 10}),
-#         label="Контент"
-#     )
-#     is_published = forms.BooleanField(
-#         label="Публикация",
-#         required=False,
-#         initial=True
-#     )
-#     cat = forms.ModelChoiceField(
-#         queryset=Category.objects.all(), 
-#         label="Категории", 
-#         empty_label="Категория не выбрана"
-#     )
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -37,6 +37,16 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         c_def = self.get_user_context(title="Добавление статьи")
         return {**context, **c_def}
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Registraion статьи")
+        return {**context, **c_def}
+
 
 class ShowPost(DataMixin, DetailView):
     model = Women
